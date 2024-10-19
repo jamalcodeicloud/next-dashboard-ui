4facb930ac957a619c65fc88ea5aeebfb21e5828
jamalcodeicloud <jamal.code@icloud.com>
2024-10-19 20:14:09 +0800

    Add Announcement component into admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from "@/app/components/Announcements";
 import AttendanceChart from "@/app/components/AttendanceChart";
 import CountChart from "@/app/components/CountChart";
 import EventCalendar from "@/app/components/EventCalendar";
@@ -37,6 +38,7 @@ const AdminPage = () => {
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
         <EventCalendar />
+        <Announcements />
       </div>
     </div>
   );
