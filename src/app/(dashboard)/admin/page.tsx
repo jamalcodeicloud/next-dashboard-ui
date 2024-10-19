261957c06dc6373fa29f7731708b0fb54ae4bb4b
jamalcodeicloud <jamal.code@icloud.com>
2024-10-19 19:29:00 +0800

    Add Bar Chart for attendance data in admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/app/components/AttendanceChart";
 import CountChart from "@/app/components/CountChart";
 import UserCard from "@/app/components/UserCard";
 
@@ -21,7 +22,9 @@ const AdminPage = () => {
           </div>
 
           {/* ATTENDANCE CHART */}
-          <div className="w-full lg:w-2/3 h-[450px]"></div>
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
         </div>
 
         {/* BOTTOM CHART */}
